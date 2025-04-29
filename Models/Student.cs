24f30efb8ee8e83e5e076408a9da0d1af005d5e7
edine-noella <edinenoella@gmail.com>
2025-04-29 12:16:43 +0200

    student Model

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+namespace StudentManager.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+
+
+    public string FullName { get; set; }
+
+    public int Age { get; set; }
+
+    public string City { get; set; }
+
+    public DateTime EnrollmentDate { get; set; }
+}
