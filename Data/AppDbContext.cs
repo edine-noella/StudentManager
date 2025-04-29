78047728b3af216be84414e71be9895a8a5dd769
edine-noella <edinenoella@gmail.com>
2025-04-29 12:19:48 +0200

    Created Entity Framework Context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using StudentManager.Models;
+namespace StudentManager.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    public DbSet<Student> Students { get; set; }
+}
