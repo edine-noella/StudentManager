8c81d9e37a35f24d57738952587a62a200bc9cd8
edine-noella <edinenoella@gmail.com>
2025-05-06 17:36:03 +0200

    bindproperty in a model doesn't make sense

--- Models/Student.cs
+++ Models/Student.cs
@@ -10,19 +10,15 @@ public class Student
 
     [Required(ErrorMessage = "full name is required !!!")]
     [StringLength(100, ErrorMessage = "full name cannot exceed 100 characters")]
-    [BindProperty]
     public string FullName { get; set; }
 
     [Range(1,120, ErrorMessage = "age must be between 1 and 120 years!")]
-    [BindProperty]
     public int Age { get; set; }
     
 
     [Required(ErrorMessage = "City is required !!!!!")]
-    [BindProperty]
     public string City { get; set; }
 
     [Required(ErrorMessage = "EnrollmentDate is required")]
-    [BindProperty]
     public DateTime EnrollmentDate { get; set; }
 }
