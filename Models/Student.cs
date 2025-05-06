a97ca3ebb9fdc1be35de7c4e8f27482a42356642
edine-noella <edinenoella@gmail.com>
2025-05-06 17:06:54 +0200

    data validations

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,15 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
 namespace StudentManager.Models;
 
 public class Student
 {
     public int Id { get; set; }
 
-
+    [Required(ErrorMessage = "full name is required !!!")]
+    [StringLength(100, ErrorMessage = "full name cannot exceed 100 characters")]
+    [BindProperty]
     public string FullName { get; set; }
 
+    [Range(1,120, ErrorMessage = "age must be between 1 and 120 years!")]
+    [BindProperty]
     public int Age { get; set; }
+    
 
+    [Required(ErrorMessage = "City is required !!!!!")]
+    [BindProperty]
     public string City { get; set; }
 
+    [Required(ErrorMessage = "EnrollmentDate is required")]
+    [BindProperty]
     public DateTime EnrollmentDate { get; set; }
 }
